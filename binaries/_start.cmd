1c50e91e8f3ea5c075e1d0a63f42192142044178
petr.janecek <petr.janecek@saabczech.cz>
2014-03-29 07:18:17 +0100

    Platform unified to AnyCPU; Release build set up.

--- binaries/_start.cmd
+++ binaries/_start.cmd
@@ -3,7 +3,7 @@ agentcmd.exe StopPlan
 taskkill /im master.exe
 taskkill /im agent.exe
 
-timeout /t 3
+timeout /t 5
 
 start master
 start agent
--- binaries/_update.cmd
+++ binaries/_update.cmd
@@ -1,17 +1,20 @@
+SET CFG=%1
+IF "%1"=="" SET CFG=Debug
+
 
 copy ..\src\packages\CommandLineParser.1.9.71\lib\net35\CommandLine.dll  .\
 copy ..\src\packages\log4net.2.0.3\lib\net35-full\log4net.dll  .\
 
-copy ..\src\Dirigent.Agent.Core\bin\Debug\Dirigent.Agent.Core.dll .\
+copy ..\src\Dirigent.Agent.Core\bin\%CFG%\Dirigent.Agent.Core.dll .\
 
-copy ..\src\Dirigent.Agent.Gui\bin\Debug\Dirigent.Agent.Gui.dll .\
+copy ..\src\Dirigent.Agent.Gui\bin\%CFG%\Dirigent.Agent.Gui.dll .\
 
-copy ..\src\Dirigent.Common\bin\Debug\Dirigent.Common.dll  .\
+copy ..\src\Dirigent.Common\bin\%CFG%\Dirigent.Common.dll  .\
 
-copy ..\src\Dirigent.Master\bin\Debug\Master.exe  .\
+copy ..\src\Dirigent.Master\bin\%CFG%\Master.exe  .\
 
-copy ..\src\Dirigent.Agent.TrayApp\bin\Debug\agent.exe .\
+copy ..\src\Dirigent.Agent.TrayApp\bin\%CFG%\agent.exe .\
 
-copy ..\src\Dirigent.Agent.CmdLineCtrl\bin\Debug\agentcmd.exe .\
+copy ..\src\Dirigent.Agent.CmdLineCtrl\bin\%CFG%\agentcmd.exe .\
 
 
