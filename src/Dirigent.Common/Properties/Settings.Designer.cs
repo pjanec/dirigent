2d531173bf03a5129d4762cf8813313983862d78
Petr Janecek <petr.janecek@bagirasys.com>
2021-06-29 13:36:13 +0200

    App settings code no longer linked from common to another projects, now a public part of common.

--- src/Dirigent.Common/Properties/Settings.Designer.cs
+++ src/Dirigent.Common/Properties/Settings.Designer.cs
@@ -14,7 +14,7 @@ namespace Dirigent.Common.Properties
 
 	[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
 	[global::System.CodeDom.Compiler.GeneratedCodeAttribute( "Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0" )]
-	internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+	public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
 	{
 
 		private static Settings defaultInstance = ( ( Settings )( global::System.Configuration.ApplicationSettingsBase.Synchronized( new Settings() ) ) );
