69dc9bc62cafb4e1f1f39ef52eeb8680174aab55
Petr Janecek <petr.janecek@bagirasys.com>
2023-10-19 12:59:58 +0200

    make_release.ps1 zips windows binaries

--- make_release.ps1
+++ make_release.ps1
@@ -7,10 +7,12 @@ Param( $buildconf="Release" )
 $version = (Get-Content -path "$PSScriptRoot\version.txt")
 & "$PSScriptRoot\gen-ver-stamp.ps1" "$PSScriptRoot\VersionStamp.txt" $version
 
+$exePath = "$PSScriptRoot\release\win-x64\$buildconf"
+
 # set the verison for the build processes started below
 $env:RELEASE_VERSION = $version
 
-# rebuld & publish for all platforms
+# rebuild & publish for all platforms
 
 & "$PSScriptRoot\publish-linux-x64.ps1" $buildconf
 
@@ -18,5 +20,14 @@ $env:RELEASE_VERSION = $version
 
 & "$PSScriptRoot\publish-win-x64.ps1" $buildconf
 
+# make zip for windows binaries
+$zippath = "$PSScriptRoot\Dirigent-$version-win-x64.7z"
+Remove-Item -Path $zippath -Force -ErrorAction SilentlyContinue
+& "C:\Program Files\7-Zip\7z.exe" a -r -t7z $zippath "$exePath\*" "-xr!*.log" "-xr!*.config" "-xr!*.zip" "-xr!*.7z"
+
+# make zip for sample configs
+$zippath = "$PSScriptRoot\Dirigent-$version-win-x64-configs.7z"
+Remove-Item -Path $zippath -Force -ErrorAction SilentlyContinue
+& "C:\Program Files\7-Zip\7z.exe" a -t7z $zippath "$exePath\*.config"
 
 
